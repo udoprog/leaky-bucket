7aae803d564493df71226c0a4555a8380c83a4ff
John-John Tedro <udoprog@tedro.se>
2020-09-27 05:25:46 +0200

    Support shutdown of task associated with a single LeakyBucket

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# A leaky-bucket rate limiter
+# leaky-bucket
 
 [![Documentation](https://docs.rs/leaky-bucket/badge.svg)](https://docs.rs/leaky-bucket)
 [![Crates](https://img.shields.io/crates/v/leaky-bucket.svg)](https://crates.io/crates/leaky-bucket)
@@ -6,17 +6,17 @@
 
 A token-based rate limiter based on the [leaky bucket] algorithm.
 
-[leaky bucket]: https://en.wikipedia.org/wiki/Leaky_bucket
+If the tokens are already available, the acquisition will be instant through
+a fast path, and the acquired number of tokens will be added to the bucket.
 
-## Usage
+If the bucket overflows (i.e. goes over max), the task that tried to acquire
+the tokens will be suspended until the required number of tokens has been
+added.
 
-This library requires the user to add the following dependencies to use:
+### Example
 
-```toml
-leaky-bucket = "0.7.1"
-```
-
-## Example
+If the project is built with the `static` feature (default), you can use
+[LeakyBucket] directly as long as you are inside a tokio runtime, like so:
 
 ```rust
 use leaky_bucket::LeakyBucket;
@@ -25,24 +25,29 @@ use std::{error::Error, time::Duration};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let rate_limiter = LeakyBucket::builder()
-        .max(100)
-        .refill_interval(Duration::from_secs(10))
-        .refill_amount(100)
+        .max(5)
+        .tokens(5)
         .build()?;
 
     println!("Waiting for permit...");
-    // should take about ten seconds to get a permit.
-    rate_limiter.acquire_one().await?;
+    // should take about 5 seconds to acquire.
+    rate_limiter.acquire(10).await?;
     println!("I made it!");
-
     Ok(())
 }
 ```
 
-## Example with custom coordinator
+Note that only one coordinator instance will be lazily created, and it's
+associated with the runtime from within it was constructed. To use multiple
+runtimes you must use an explicit coordinator as documented in the next
+section.
 
-Leaky buckets require coordination. By default, this will happen through a static coordinator spawned through `tokio::spawn` at first use.
-If you want to spawn the coordinator yourself, you can do the following with `LeakyBuckets`:
+### Example using explicit coordinator
+
+Leaky buckets require coordination. By default, this will happen through a
+static coordinator spawned through [tokio::spawn] at first use. If you want
+to spawn the coordinator yourself, you can do the following with
+[LeakyBuckets]:
 
 ```rust
 use leaky_bucket::LeakyBuckets;
@@ -52,21 +57,31 @@ use std::{error::Error, time::Duration};
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut buckets = LeakyBuckets::new();
     let coordinator = buckets.coordinate()?;
+
     // spawn the coordinate thread to refill the rate limiter.
-    tokio::spawn(async move { coordinator.await.expect("coordinate thread errored") });
+    tokio::spawn(async move { coordinator.await.expect("coordinator errored") });
 
     let rate_limiter = buckets
         .rate_limiter()
-        .max(100)
-        .refill_interval(Duration::from_secs(10))
-        .refill_amount(100)
+        .max(5)
+        .tokens(5)
         .build()?;
 
     println!("Waiting for permit...");
-    // should take about ten seconds to get a permit.
-    rate_limiter.acquire_one().await?;
+    // should take about 5 seconds to acquire.
+    rate_limiter.acquire(10).await?;
     println!("I made it!");
-
     Ok(())
 }
 ```
+
+Note that since this coordinator uses timing facilities from tokio it has to
+be spawned within a Tokio runtime with the [`time` feature] enabled.
+
+[leaky bucket]: https://en.wikipedia.org/wiki/Leaky_bucket
+[tokio::spawn]: https://docs.rs/tokio/0/tokio/fn.spawn.html
+[LeakyBucket]: https://docs.rs/leaky-bucket/0/leaky_bucket/struct.LeakyBucket.html
+[LeakyBuckets]: https://docs.rs/leaky-bucket/0/leaky_bucket/struct.LeakyBuckets.html
+[`time` feature]: https://docs.rs/tokio/0.2.22/tokio/#feature-flags
+
+License: MIT/Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,22 @@
 #![recursion_limit = "256"]
 #![deny(missing_docs)]
-//! # A leaky-bucket rate limiter
+//! [![Documentation](https://docs.rs/leaky-bucket/badge.svg)](https://docs.rs/leaky-bucket)
+//! [![Crates](https://img.shields.io/crates/v/leaky-bucket.svg)](https://crates.io/crates/leaky-bucket)
+//! [![Actions Status](https://github.com/udoprog/leaky-bucket/workflows/Rust/badge.svg)](https://github.com/udoprog/leaky-bucket/actions)
 //!
-//! This implements a leaky bucket from which you can acquire tokens.
+//! A token-based rate limiter based on the [leaky bucket] algorithm.
 //!
-//! If the tokens are already available, the acquisition will be instant (fast path) and the
-//! acquired number of tokens will be added to the bucket.
+//! If the tokens are already available, the acquisition will be instant through
+//! a fast path, and the acquired number of tokens will be added to the bucket.
 //!
-//! If the bucket overflows (i.e. goes over max), the task that tried to acquire the tokens will
-//! be suspended until the required number of tokens has been added.
+//! If the bucket overflows (i.e. goes over max), the task that tried to acquire
+//! the tokens will be suspended until the required number of tokens has been
+//! added.
 //!
 //! ## Example
 //!
 //! If the project is built with the `static` feature (default), you can use
-//! `LeakyBucket` directly as long as you are inside a tokio runtime, like so:
+//! [LeakyBucket] directly as long as you are inside a tokio runtime, like so:
 //!
 //! ```no_run
 //! use leaky_bucket::LeakyBucket;
@@ -34,7 +37,17 @@
 //! }
 //! ```
 //!
+//! Note that only one coordinator instance will be lazily created, and it's
+//! associated with the runtime from within it was constructed. To use multiple
+//! runtimes you must use an explicit coordinator as documented in the next
+//! section.
+//!
 //! ## Example using explicit coordinator
+//!
+//! Leaky buckets require coordination. By default, this will happen through a
+//! static coordinator spawned through [tokio::spawn] at first use. If you want
+//! to spawn the coordinator yourself, you can do the following with
+//! [LeakyBuckets]:
 //!
 //! ```no_run
 //! use leaky_bucket::LeakyBuckets;
@@ -44,8 +57,9 @@
 //! async fn main() -> Result<(), Box<dyn Error>> {
 //!     let mut buckets = LeakyBuckets::new();
 //!     let coordinator = buckets.coordinate()?;
+//!
 //!     // spawn the coordinate thread to refill the rate limiter.
-//!     tokio::spawn(async move { coordinator.await.expect("coordinate thread errored") });
+//!     tokio::spawn(async move { coordinator.await.expect("coordinator errored") });
 //!
 //!     let rate_limiter = buckets
 //!         .rate_limiter()
@@ -60,16 +74,21 @@
 //!     Ok(())
 //! }
 //! ```
+//!
+//! Note that since this coordinator uses timing facilities from tokio it has to
+//! be spawned within a Tokio runtime with the [`time` feature] enabled.
+//!
+//! [leaky bucket]: https://en.wikipedia.org/wiki/Leaky_bucket
+//! [tokio::spawn]: https://docs.rs/tokio/0/tokio/fn.spawn.html
+//! [LeakyBucket]: https://docs.rs/leaky-bucket/0/leaky_bucket/struct.LeakyBucket.html
+//! [LeakyBuckets]: https://docs.rs/leaky-bucket/0/leaky_bucket/struct.LeakyBuckets.html
+//! [`time` feature]: https://docs.rs/tokio/0.2.22/tokio/#feature-flags
 
 use futures_util::stream::FuturesUnordered;
-use std::{
-    future::Future,
-    sync::{
-        atomic::{AtomicUsize, Ordering},
-        Arc,
-    },
-    time::Duration,
-};
+use std::future::Future;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::time::Duration;
 use thiserror::Error;
 use tokio::stream::StreamExt as _;
 use tokio::sync::{mpsc, oneshot};
@@ -238,7 +257,6 @@ impl Builder<'_> {
             max,
             refill_interval,
             refill_amount,
-            task_tx,
         });
 
         self.new_task_tx
@@ -249,7 +267,7 @@ impl Builder<'_> {
             })
             .map_err(|_| Error::NewTaskError)?;
 
-        Ok(LeakyBucket { inner })
+        Ok(LeakyBucket { inner, task_tx })
     }
 }
 
@@ -280,8 +298,6 @@ struct Inner {
     refill_interval: Duration,
     /// Amount to add when refilling.
     refill_amount: usize,
-    /// Sender for emitting queued tasks.
-    task_tx: mpsc::Sender<Task>,
 }
 
 impl std::fmt::Debug for Inner {
@@ -378,6 +394,8 @@ impl Inner {
 #[derive(Clone)]
 pub struct LeakyBucket {
     inner: Arc<Inner>,
+    /// Sender for emitting queued tasks.
+    task_tx: mpsc::Sender<Task>,
 }
 
 impl std::fmt::Debug for LeakyBucket {
@@ -555,8 +573,7 @@ impl LeakyBucket {
 
         let (completion, completion_rx) = oneshot::channel();
 
-        self.inner
-            .task_tx
+        self.task_tx
             .clone()
             .send(Task {
                 required,
